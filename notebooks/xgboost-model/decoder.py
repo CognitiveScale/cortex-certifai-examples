0aa3f88ffd5d960ccdc303fae052b4a96ebf6648
akumar-cs <akumar@cognitivescale.com>
2020-05-22 21:52:01 +0530

    added few tests

---
+++ notebooks/xgboost-model/decoder.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class DecoderCertifai:
+    def __init__(self,threshold):
+        self.threshold = threshold
+    
+    def __call__(self,x):
+        if not isinstance(x, np.ndarray):
+             x = np.array(x)
+        return (x > self.threshold).astype(int)
--- notebooks/xgboost-model/predict_transform.py
+++ notebooks/xgboost-model/predict_transform.py
@@ -5,8 +5,10 @@ import xgboost as xgb
 class TransformedPredict:
     def __init__(self,model,dropped_indexes_list):
         self.model = model
+        self.dropped_indexes_list = dropped_indexes_list
         
     def predict(self,arr):
-        dtest = xgb.DMatrix(data=np.delete(arr, self.dropped_indexes_list,1))
+        # data = np.delete(arr, self.dropped_indexes_list,1)
+        dtest = xgb.DMatrix(data=arr)
         return self.model.predict(dtest)
         
