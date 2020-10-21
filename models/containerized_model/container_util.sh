53c8105519e49d88c3115f1aef22e394912f9f16
Mayank Chutani <mchutani@cognitivescale.com>
2020-10-21 10:57:34 +0530

    run step to expect env file, updated readme

--- models/containerized_model/container_util.sh
+++ models/containerized_model/container_util.sh
@@ -8,7 +8,12 @@ COMMAND=$1
 if [ "$COMMAND" = "build" ]; then
   docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
 elif [ "$COMMAND" = "run" ]; then
-  docker run -p 8551:8551 --env-file environment.yml -it $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
+  ENV_PATH=$2
+  if [ "$ENV_PATH" = "" ]; then
+    echo "Pass the environments file as the second argument. e.g /.container_util.sh run ./environment.yml"
+    exit 1
+  fi
+  docker run -p 8551:8551 --env-file $ENV_PATH -it $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG
 else
   echo "Param should be one of [\"build\", \"run\"]"
 fi
