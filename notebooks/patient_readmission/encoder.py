0fe1849d006e5b9a030e6d1d3db6db24bbb49a21
Christine Draper <cdraper@cognitivescale.com>
2020-11-02 14:29:38 -0600

    Remove 1.3.5 workaround

--- notebooks/patient_readmission/encoder.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-# This encoder works round an issue (fix in progress) where Certifai is converting ints to floats
-# The symptom of this issue is that some counterfactuals appear to have no changed features
-# All the fields in this example should be ints
-class Encoder:
-  def __call__(self, predictions):
-    return np.rint(predictions)
