a5bf52c825ec03286fc8ac0813c3147b6de7af64
Christine Draper <cdraper@cognitivescale.com>
2020-12-03 09:06:17 -0600

    Add xgboost iris example (#75)
    * Add xgboost iris example
    
    * Review comments and fix so labels are mapped properly

--- models/iris/app_xgb.py
+++ models/iris/app_xgb.py
@@ -6,23 +6,30 @@ Licensed under CognitiveScale Example Code License https://github.com/CognitiveS
 from certifai.model.sdk import SimpleModelWrapper
 import pickle
 import numpy as np
+import xgboost as xgb
 
 # The model returns 0, 1, or 2. The service should return the names of the
-# species
+# species. The score labels must be given in the order of the probabilities
+# returned by the model.
 species = np.array(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'])
 
 # Customize the SimpleModelWrapper to convert the predictions to the species
 class IrisApp(SimpleModelWrapper):
     def predict(self, npinstances):
+        # This method is only used if supports_soft_scores is False
         results = self.model.predict(npinstances)
         return species[results]
 
 # Load the trained model and its encoder
-with open('iris_svm.pkl', 'rb') as f:
+with open('iris_xgb.pkl', 'rb') as f:
     saved = pickle.load(f)
     model = saved.get('model')
     encoder = saved.get('encoder', None)
 
 # Run the wrapped model as a service
-app = IrisApp(model=model, encoder=encoder)
+app = IrisApp(model=model,
+    encoder=encoder,
+    supports_soft_scores=True,
+    score_labels=species.tolist()
+)
 app.run()
