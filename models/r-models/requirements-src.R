586d198607fc5d506dc6c98810882fad423563bd
akumar-cs <akumar@cognitivescale.com>
2020-12-16 12:34:50 +0530

    r model template containerization work

---
+++ models/r-models/requirements-src.R
@@ -0,0 +1,6 @@
+# non-binary packages to be installed go here.
+# note: these packages are built from source
+
+# install.packages("randomForest")
+# install.packages("caret")
+# install.packages('plumber')
--- models/r-models/run_server.R
+++ models/r-models/run_server.R
@@ -3,10 +3,6 @@ Copyright (c) 2020. Cognitive Scale Inc. All rights reserved.
 Licensed under CognitiveScale Example Code License https://github.com/CognitiveScale/cortex-certifai-examples/blob/master/LICENSE.md
 "
 
-if (!require(caret)) {
-  install.packages("plumber")
-}
-
 library(plumber)
-pr <- plumber::plumb("predict_german_credit.R")
-pr$run(port=8551)
+pr <- plumber::plumb("/src/predict_german_credit.R")
+pr$run(host="0.0.0.0" ,port=8551)
